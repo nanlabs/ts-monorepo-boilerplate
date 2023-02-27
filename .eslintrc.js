035166bece9edd45a1cd0b060bc9e5229a0ed766
ulises-jeremias <ulisescf.24@gmail.com>
2023-02-27 12:54:12 -0300

    Updated packages and eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: ["custom"],
+  parserOptions: {
+    sourceType: "module",
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+};
